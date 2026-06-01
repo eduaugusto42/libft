6c26687878e985419915a9fff26c6d86aa79ec89
Eduardo <eduaugusto@proton.me>
2026-06-01 09:49:44 -0300

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,37 @@
 /*   By: eduaaugu <eduaaugu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 09:03:47 by eduaaugu          #+#    #+#             */
-/*   Updated: 2026/06/01 09:11:43 by eduaaugu         ###   ########.fr       */
+/*   Updated: 2026/06/01 09:49:24 by eduaaugu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strjoin(char const *s1, char const *s2);
+#include <stddef.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(char const	*s);
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*sjoin;
+	size_t	i;
+	size_t	j;
+
+	i = ft_strlen(s1) + ft_strlen(s2);
+	sjoin = malloc(i + 1);
+	if (sjoin == 	NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		sjoin[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		sjoin[i + j] = s2[j];
+		j++;
+	}
+	sjoin[i + j] = '\0';
+	return (sjoin);
+}
