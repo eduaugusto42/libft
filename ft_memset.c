bf9301673c132df8226276aabcc1149bc3bb64da
Eduardo Augusto <eduaugusto@proton.me>
2026-06-02 13:30:42 -0300

    remaining functions from part 1

---
+++ ft_memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduaaugu <eduaaugu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 16:25:51 by eduaaugu          #+#    #+#             */
+/*   Updated: 2026/05/25 16:43:36 by eduaaugu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)s;
+	while (n > 0)
+	{
+		*p = (unsigned char)c;
+		p++;
+		n--;
+	}
+	return (s);
+}
